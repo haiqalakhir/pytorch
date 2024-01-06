87f9598af1645dfcbf6d200ed781188a68385e27
oppanjang <mohamadhaiqalakhir@yahoo.com>
2024-01-06 15:59:39 +0800

    Update differentiation.py

--- torch/pytorch13/differentiation.py
+++ torch/pytorch13/differentiation.py
@@ -5,4 +5,10 @@ y = x**2
 
 jo = y.backward()
 
-print(x.grad)
+print(x.grad)  # dy/dx(x=2) = x^2
+
+
+xx = torch.tensor(2.0,requires_grad=True)
+zz = xx**2+2*xx+1
+jon = zz.backward()
+print(xx.grad)
